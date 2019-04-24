cf4bdb184a1501b1f523875dacd0b0479425744f
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-04-24 19:57:22 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   def name=(dog_name) #setting #writer
     @this_dogs_name = dog_name
   end
- 
+
   def name #getting #reader
     @this_dogs_name
   end
@@ -10,5 +10,5 @@ end
 
 lassie = Dog.new
 lassie.name = "Lassie"
- 
+
 puts lassie.name #=> "Lassie"
